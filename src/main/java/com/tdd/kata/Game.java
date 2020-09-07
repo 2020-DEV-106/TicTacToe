f457371bdaf0115a801d9844efcf37f4b35dce5c
2020-DEV-106 <106dev2020@gmail.com>
2020-09-07 22:49:40 +0530

    extracted methods getCurrentPlayer and setPreviousPlayer; introduced enum Player to replace constants PLAYER_X and PLAYER_O

--- src/main/java/com/tdd/kata/Game.java
+++ src/main/java/com/tdd/kata/Game.java
@@ -3,15 +3,21 @@ package com.tdd.kata;
 public class Game {
 
     private static final int GRID_SIZE = 3;
-    private static final char PLAYER_X = 'X';
-    private static final char PLAYER_O = 'O';
     private final char[][] board = new char[GRID_SIZE][GRID_SIZE];
     private char previousPlayer;
 
     public void playAt(int row, int column) {
-        char currentPlayer = previousPlayer == PLAYER_X ? PLAYER_O : PLAYER_X;
+        char currentPlayer = getCurrentPlayer();
         board[row][column] = currentPlayer;
+        setPreviousPlayer(currentPlayer);
+    }
+
+    private void setPreviousPlayer(char currentPlayer) {
         previousPlayer = currentPlayer;
+    }
+
+    private char getCurrentPlayer() {
+        return previousPlayer == Player.PLAYER_X.value ? Player.PLAYER_O.value : Player.PLAYER_X.value;
     }
 
     public char getPlayerAt(int row, int column) {
--- src/test/java/com/tdd/kata/GameTest.java
+++ src/test/java/com/tdd/kata/GameTest.java
@@ -10,8 +10,6 @@ public class GameTest {
 
     private static final int POSITION_ZERO = 0;
     private static final int POSITION_ONE = 1;
-    private static final char PLAYER_X = 'X';
-    private static final char PLAYER_O = 'O';
     private Game game;
 
     @Before
@@ -23,7 +21,7 @@ public class GameTest {
     public void getPlayerAtShouldReturnXAfterFirstTurn() {
         game.playAt(POSITION_ZERO, POSITION_ZERO);
 
-        assertThat(game.getPlayerAt(POSITION_ZERO, POSITION_ZERO), is(PLAYER_X));
+        assertThat(game.getPlayerAt(POSITION_ZERO, POSITION_ZERO), is(Player.PLAYER_X.value));
     }
 
     @Test
@@ -31,6 +29,6 @@ public class GameTest {
         game.playAt(POSITION_ZERO, POSITION_ZERO);
         game.playAt(POSITION_ZERO, POSITION_ONE);
 
-        assertThat(game.getPlayerAt(POSITION_ZERO, POSITION_ONE), is(PLAYER_O));
+        assertThat(game.getPlayerAt(POSITION_ZERO, POSITION_ONE), is(Player.PLAYER_O.value));
     }
 }
