d093cc372cbf85b219120f3ee0ffa5c1c1121765
2020-DEV-106 <106dev2020@gmail.com>
2020-09-07 22:29:50 +0530

    verified if second player is O after second turn

--- src/test/java/com/tdd/kata/GameTest.java
+++ src/test/java/com/tdd/kata/GameTest.java
@@ -17,4 +17,14 @@ public class GameTest {
 
         Assert.assertThat(game.getPlayerAt(POSITION_ZERO, POSITION_ZERO), Is.is(PLAYER_X));
     }
+
+    @Test
+    public void getPlayerAtShouldReturnOAfterSecondTurn() {
+        Game game = new Game();
+
+        game.playAt(POSITION_ZERO, POSITION_ZERO);
+        game.playAt(POSITION_ZERO, 1);
+
+        Assert.assertThat(game.getPlayerAt(POSITION_ZERO, 1), Is.is('O'));
+    }
 }
