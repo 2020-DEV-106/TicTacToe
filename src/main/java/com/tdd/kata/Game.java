49014a030375a967e76966a5eb0c5a3b177ab849
2020-DEV-106 <106dev2020@gmail.com>
2020-09-08 01:34:17 +0530

    reused method isThirdColumnFilledByPlayerO and removed unused method isThirdColumnFilledByPlayerX

--- src/main/java/com/tdd/kata/Game.java
+++ src/main/java/com/tdd/kata/Game.java
@@ -39,7 +39,7 @@ public class Game {
         if (isAnyRowFilledByPlayer(PLAYER_X.value)
                 || isFirstColumnFilledByPlayer(PLAYER_X.value)
                 || isSecondColumnFilledByPlayer(PLAYER_X.value)
-                || isThirdColumnFilledByPlayerX()) {
+                || isThirdColumnFilledByPlayerO(PLAYER_X.value)) {
             winner = PLAYER_X.value;
         }
 
@@ -57,12 +57,6 @@ public class Game {
         return getPlayerAt(POSITION_ZERO, POSITION_TWO) == playerO
                 && getPlayerAt(POSITION_ONE, POSITION_TWO) == playerO
                 && getPlayerAt(POSITION_TWO, POSITION_TWO) == playerO;
-    }
-
-    private boolean isThirdColumnFilledByPlayerX() {
-        return getPlayerAt(POSITION_ZERO, POSITION_TWO) == PLAYER_X.value
-                && getPlayerAt(POSITION_ONE, POSITION_TWO) == PLAYER_X.value
-                && getPlayerAt(POSITION_TWO, POSITION_TWO) == PLAYER_X.value;
     }
 
     private boolean isSecondColumnFilledByPlayer(char player) {
