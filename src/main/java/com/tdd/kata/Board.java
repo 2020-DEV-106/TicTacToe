968ffbfce5bad1d2613941a297a1d1fc24215f2c
2020-DEV-106 <106dev2020@gmail.com>
2020-09-09 09:05:32 +0530

    extracted constant INITIAL_VALUE_OF_POSITION_IN_BOARD

--- src/main/java/com/tdd/kata/Board.java
+++ src/main/java/com/tdd/kata/Board.java
@@ -5,6 +5,7 @@ public class Board {
     private static final int GRID_SIZE = 3;
     private static final int TOTAL_NUMBER_CELLS_IN_BOARD = 9;
     private static final int NUMBER_OF_CELLS_FILLED_BEFORE_GAME_STARTS = 0;
+    private static final char INITIAL_VALUE_OF_POSITION_IN_BOARD = '\0';
     private final char[][] gameBoard;
     private int numberOfCellsFilled;
 
@@ -27,6 +28,6 @@ public class Board {
     }
 
     public boolean isPositionOccupied(int row, int column) {
-        return gameBoard[row][column] != '\0';
+        return gameBoard[row][column] != INITIAL_VALUE_OF_POSITION_IN_BOARD;
     }
 }
