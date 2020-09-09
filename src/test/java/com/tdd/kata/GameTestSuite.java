77498f87432cdb18a2a46ce65c174d6bbec44ba5
2020-DEV-106 <106dev2020@gmail.com>
2020-09-09 10:07:30 +0530

    added test suite class

---
+++ src/test/java/com/tdd/kata/GameTestSuite.java
@@ -0,0 +1,11 @@
+package com.tdd.kata;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({BoardTest.class, GameTest.class, GameRunnerTest.class})
+public class GameTestSuite {
+
+}
